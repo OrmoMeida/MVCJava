69e050720818010c5e9bf41a1b2f3865ffb65e28
AmeMeida <ame.pistache@gmail.com>
2022-09-09 08:51:19 -0300

    Mensagens de erro padrão

--- MVCMusicas/src/pExceptions/MusicNotFoundException.java
+++ MVCMusicas/src/pExceptions/MusicNotFoundException.java
@@ -3,8 +3,13 @@ package pExceptions;
 import java.util.NoSuchElementException;
 
 public class MusicNotFoundException extends NoSuchElementException {
+    public static final String NotFoundByName   = "Nome de música não encontrado na busca.";
+    public static final String NotFoundByAuthor = "Autor não encontrado na busca.";
+    public static final String NotFound         = "Música não encontrada na busca.";
+    public static final String EmptySearchSet   = "Nenhuma música encontrada na busca."; 
+
     public MusicNotFoundException() {
-        super("Nome de música não encontrado na pesquisa.");
+        super(NotFound);
     }
 
     public MusicNotFoundException(String s) {
